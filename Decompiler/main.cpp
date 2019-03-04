24cf13c437656dd2745f403efbef151370b77ca4
Manuel Deneu <manueldeneu@gmail.com>
2019-03-04 09:11:27 +0100

    sync

--- Decompiler/main.cpp
+++ Decompiler/main.cpp
@@ -67,6 +67,7 @@ int main(int argc, const char * argv[])
         printf("\n");
         */
         
+        
         AML::Decompiler decomp;
         
         auto ret = decomp.process(buffer, bufSize);
